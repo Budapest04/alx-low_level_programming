92083b55b9401038541875dca9c560ab7c231797
Budapest04 <corporatebagudu@gmail.com>
2022-09-22 03:20:47 -0700

    Task 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
