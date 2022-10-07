747a8e4b79e3763ae4731d325e73f58874b980ba
Budapest04 <111089262+Budapest04@users.noreply.github.com>
2022-10-07 23:37:40 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdlib.h>
 
-#include <stdio.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: size in bytes
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *arr;
 
-int _putchar(char c);
-void *malloc(size_t size);
-void free(void *ptr);
-void exit(int status);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-#endif /* MAIN_H */
+	arr = malloc(b);
+	if (arr == NULL)
+		exit(98);
+	return (arr);
+}
